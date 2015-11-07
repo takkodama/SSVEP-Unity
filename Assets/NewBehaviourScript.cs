c2188bee474c6663f67ec9c69ce2d8d57a07c8e5
shartsu <shartsu.in@gmail.com>
2015-11-07 15:49:16 +0100

    Put a picture on the screen and make them flicker randomly

--- Assets/NewBehaviourScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript : MonoBehaviour {
-	void OnGUI(){
-		GUI.Button(new Rect(10,10,100,25), "Click me");
-	}
-}
---
+++ Assets/prof.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prof : MonoBehaviour {
+	
+	private Renderer _renderer; 
+	
+	// Use this for initialization
+	void Start () {
+		_renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Random.Range (0,3) == 0) {
+			kuso ();
+		} else {
+			yaya();
+		}
+	}
+	
+	
+	void kuso(){
+		
+		_renderer.enabled = false; //trueで表示
+		
+	}
+	void yaya(){
+		
+		_renderer.enabled = true;
+	}
+}
