cd31a2bb40dacd35c09cac0c7c02710fb856d3cd
shartsu <shartsu.in@gmail.com>
2015-11-08 21:50:14 +0100

    Made the stimuli picture flicker as 30 Hz and then tried some code to control a frequency precisely

--- Assets/prof.cs
+++ Assets/prof.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class prof : MonoBehaviour {
 	
 	private Renderer _renderer; 
-	
+	int counter = 1; //set as 1 intentionally (frame would start from 1 firstly)
+
 	// Use this for initialization
 	void Start () {
 		_renderer = GetComponent<Renderer>();
@@ -12,22 +13,41 @@ public class prof : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (Random.Range (0,3) == 0) {
-			kuso ();
-		} else {
-			yaya();
+
+		//Is it accessed? or automatically reset when the number reached its maximum flame number (ex.FPS=60)?
+		if (counter == 61) {
+			counter = 1;
 		}
+
+		//30Hz
+		if (counter > 30) {
+			onpic();
+		} else {
+			offpic();
+		}
+
+		//15Hz
+		/*
+		if (counter % 4 == 0) {
+			onpic();
+		} else {
+			offpic();
+		}
+		*/
+
+		counter += 1;
 	}
 	
-	
-	void kuso(){
-		
-		_renderer.enabled = false; //trueで表示
-		
-	}
-	void yaya(){
+
+	void onpic(){
 		
 		_renderer.enabled = true;
 	}
+
+	void offpic(){
+
+		_renderer.enabled = false;
+
+	}
+
 }
