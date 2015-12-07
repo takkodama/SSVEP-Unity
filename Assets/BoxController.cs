5ae468543374c3338f4979db3ae8b5e5597eaded
shartsu <shartsu.in@gmail.com>
2015-12-07 13:14:47 +0100

    Create BoxFlicker class

--- Assets/BoxController.cs
+++ Assets/BoxController.cs
@@ -6,51 +6,49 @@ public class BoxController : MonoBehaviour {
 
 	//Declare UDPReceiver
 	private UDPReceiver udprcv;
-	public Text text_PORT1;
-	public Text text_PORT2;
-	public Text text_PORT3;
-	public Text text_PORT4;
-
-	//private string portNum;
-
+	
+	//Boxes
 	public GameObject systemObj1;
 	public GameObject systemObj2;
 	public GameObject systemObj3;
 	public GameObject systemObj4;
+
+	//Boxes & Flicer instances
+	public Image box1;
+	public Image box2;
+	public Image box3;
+	public Image box4;
+	private BoxFlicker box_10hz;
+	private BoxFlicker box_12hz;
+	private BoxFlicker box_15hz;
+	private BoxFlicker box_20hz;
+
+	//Frame counter (NOT debug)
+	private int flagMan = 0;
+	
+	//(For debug) Each boxes counter
 	public GameObject systemObj5;
 	public GameObject systemObj6;
 	public GameObject systemObj7;
 	public GameObject systemObj8;
 	public GameObject systemObj9;
 	public GameObject systemObj10;
-
-	//Texts for port1~4
-	public GameObject systemObj11;
-	public GameObject systemObj12;
-	public GameObject systemObj13;
-	public GameObject systemObj14;
-
-	public Image box1;
-	public Image box2;
-	public Image box3;
-	public Image box4;
 	public Text text1;
 	public Text text2;
-
 	public Text text10;
 	public Text text12;
 	public Text text15;
 	public Text text20;
 
-	private float updateDuration;
-	private int updateFrameCounter;
-	private int updateFrameCounter10Hz;
-	private int updateFrameCounter12Hz;
-	private int updateFrameCounter15Hz;
-	private int updateFrameCounter20Hz;
-	
-	private int flagMan;
+	//(For debug) Received PORT number
+	public GameObject systemObj11, systemObj12, systemObj13, systemObj14;
+	public Text text_PORT1, text_PORT2, text_PORT3, text_PORT4;
 
+	//(For debug) Frame Counter and Elapsed Time
+	private float updateDuration = 0.0f;
+	private int updateFrameCounter = 0;
+
+	//Flicker pattern arrays
 	public int[] pattern30 = new int[] {
 		0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
 		0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
@@ -98,57 +96,47 @@ public class BoxController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//Define frame rate
 		Application.targetFrameRate = 60;
 
-		//Set UDPReceiver instance
+		//Set UDPReceiver instance & Port number set
 		udprcv = GetComponent<UDPReceiver> ();
-
-		//Port number set
-		//Later set == that value will be set finally
 		udprcv.PORT_SET (22222, 22223, 22224, 22225);
 
-		box1 = systemObj1.GetComponent<Image>();
+		//Set GetComponents (must be put here, or Start() function)
+		box1 = systemObj1.GetComponent<Image>(); 
 		box2 = systemObj2.GetComponent<Image>();
 		box3 = systemObj3.GetComponent<Image>();
 		box4 = systemObj4.GetComponent<Image>();
-
-		//Alpha (Firstly Off)
-		box1.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-		box2.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-		box3.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-		box4.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-		//Check On-Off
-
+		//(For debug)
 		text1 = systemObj5.GetComponent<Text> ();
 		text2 = systemObj6.GetComponent<Text> ();
-
 		text10 = systemObj7.GetComponent<Text> ();
 		text12 = systemObj8.GetComponent<Text> ();
 		text15 = systemObj9.GetComponent<Text> ();
 		text20 = systemObj10.GetComponent<Text> ();
-
 		text_PORT1 = systemObj11.GetComponent<Text> ();
 		text_PORT2 = systemObj12.GetComponent<Text> ();
 		text_PORT3 = systemObj13.GetComponent<Text> ();
 		text_PORT4 = systemObj14.GetComponent<Text> ();
-	
-		updateDuration = 0.0f;
-		updateFrameCounter = 0;
-		updateFrameCounter10Hz = 0;
-		updateFrameCounter12Hz = 0;
-		updateFrameCounter15Hz = 0;
-		updateFrameCounter20Hz = 0;
 
-		flagMan = 0;
+		//Set BoxFlicker instance
+		box_10hz = systemObj1.AddComponent<BoxFlicker>(); 
+		box_12hz = systemObj2.AddComponent<BoxFlicker>(); 
+		box_15hz = systemObj3.AddComponent<BoxFlicker>(); 
+		box_20hz = systemObj4.AddComponent<BoxFlicker>(); 
 
+		box_10hz.Setting (pattern10, box1);
+		box_12hz.Setting (pattern12, box2);
+		box_15hz.Setting (pattern15, box3);
+		box_20hz.Setting (pattern20, box4);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Debug.Log ("========= For Debug =========");
 	
-		//Call UDPReceiver 
-		//!Action! Not sure whether it should be set here
+		//(For Debug) Call UDPReceiver 
 		Debug.Log ("udprcv(22222):" + udprcv.PORT_GET_1 ());
 		Debug.Log ("udprcv(22223):" + udprcv.PORT_GET_2 ());
 		Debug.Log ("udprcv(22224):" + udprcv.PORT_GET_3 ());
@@ -160,11 +148,9 @@ public class BoxController : MonoBehaviour {
 		text_PORT3.text = udprcv.PORT_GET_3 ();
 		text_PORT4.text = udprcv.PORT_GET_4 ();
 
+		//(For Debug) Frame Counter and Elapsed Time
 		updateDuration += Time.deltaTime;
-		//Debug.Log("updateDuration :" +  updateDuration);
-		
 		++updateFrameCounter;
-		//Debug.Log("updateFrameCounter :" +  updateFrameCounter);
 
 		if (updateFrameCounter % 60 == 0) {
 			text1.text = updateDuration.ToString ();
@@ -172,70 +158,19 @@ public class BoxController : MonoBehaviour {
 		}
 
 		++flagMan;
-		if (flagMan == 60) {
+		if (flagMan == 60) 
 			flagMan = 0;
 
-			//Counter to assure flashing frequencies for each boxes on production
-			updateFrameCounter10Hz = 0;
-			updateFrameCounter12Hz = 0;
-			updateFrameCounter15Hz = 0;
-			updateFrameCounter20Hz = 0;
+		box_10hz.Box (flagMan);
+		box_12hz.Box (flagMan);
+		box_15hz.Box (flagMan);
+		box_20hz.Box (flagMan);
 
-		}
-
-		//10Hz
-		if (pattern10 [flagMan] == 1) {
-			if(pattern10 [flagMan-1] == 0)
-				++updateFrameCounter10Hz;
-
-			text10.text = updateFrameCounter10Hz.ToString ();
-
-			box1.color = new Color(1.00f, 1.00f, 1.00f, 1.00f);
-			//Debug.Log ("pattern10[" + flagMan + "]: " + pattern10 [flagMan]);
-		} else {
-
-			box1.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-			//Debug.Log ("pattern10[" + flagMan + "]: " + pattern10 [flagMan]);
-		}
-
-		//12Hz
-		if (pattern12 [flagMan] == 1) {
-			if(pattern12 [flagMan-1] == 0)
-				++updateFrameCounter12Hz;
-			text12.text = updateFrameCounter12Hz.ToString ();
-
-			box2.color = new Color(1.00f, 1.00f, 1.00f, 1.00f);
-			//Debug.Log ("pattern12[" + flagMan + "]: " + pattern12 [flagMan]);
-		} else {
-			box2.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-			//Debug.Log ("pattern12[" + flagMan + "]: " + pattern12 [flagMan]);
-		}
-		
-		//15Hz
-		if (pattern15 [flagMan] == 1) {
-			if(pattern15 [flagMan-1] == 0)
-				++updateFrameCounter15Hz;
-			text15.text = updateFrameCounter15Hz.ToString ();
-
-			box3.color = new Color(1.00f, 1.00f, 1.00f, 1.00f);
-			//Debug.Log ("pattern15[" + flagMan + "]: " + pattern15 [flagMan]);
-		} else {
-			box3.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-			//Debug.Log ("pattern15[" + flagMan + "]: " + pattern15 [flagMan]);
-		}
-
-		//20Hz
-		if (pattern20 [flagMan] == 1) {
-			if(pattern20 [flagMan-1] == 0)
-				++updateFrameCounter20Hz;
-			text20.text = updateFrameCounter20Hz.ToString ();
-
-			box4.color = new Color(1.00f, 1.00f, 1.00f, 1.00f);
-			//Debug.Log ("pattern20[" + flagMan + "]: " + pattern20 [flagMan]);
-		} else {
-			box4.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
-			//Debug.Log ("pattern20[" + flagMan + "]: " + pattern20 [flagMan]);
-		}
+		//(For Debug) Counter to assure flashing frequencies for each boxes on production
+		text10.text = box_10hz.GetCounter ();
+		text12.text = box_12hz.GetCounter ();
+		text15.text = box_15hz.GetCounter ();
+		text20.text = box_20hz.GetCounter ();
 	
 	}
 }
---
+++ Assets/BoxFlicker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BoxFlicker : MonoBehaviour {
+
+	private int updateFrameCounter;
+	private int[] patternArray;
+	private Image box;
+
+	public void Setting(int[] c_patternArray, Image c_box)
+	{
+		this.patternArray = c_patternArray;
+		c_box.color = new Color(1.00f, 1.00f, 1.00f, 0.00f);
+		this.box = c_box;
+	}
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Box (int flagMan) {
+
+		if (flagMan == 0)
+			updateFrameCounter = 0;
+		
+		//10Hz
+		if (patternArray [flagMan] == 1) {
+			if (patternArray [flagMan - 1] == 0)
+				++updateFrameCounter;
+	
+			box.color = new Color (1.00f, 1.00f, 1.00f, 1.00f);
+			//Debug.Log ("patternArray[" + flagMan + "]: " + patternArray [flagMan]);
+		} else {
+		
+			box.color = new Color (1.00f, 1.00f, 1.00f, 0.00f);
+			//Debug.Log ("patternArray[" + flagMan + "]: " + patternArray [flagMan]);
+		}
+	}
+
+	public string GetCounter () {
+		return updateFrameCounter.ToString ();
+	}
+
+}
